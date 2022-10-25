b8c1ef815ec17762ab89e7c8acc70a7a0cf18e8d
hierrr <98255791+hierrr@users.noreply.github.com>
2022-10-25 21:51:41 +0900

    app signin/up

--- app.py
+++ app.py
@@ -1,42 +1,69 @@
 from http import client
-from flask import render_template, Flask, jsonify, request, session, redirect
+from types import NoneType
+from flask import Flask, session, request, render_template, redirect
 from pymongo import MongoClient
 
-client = MongoClient("localhost", 27017)
+client = MongoClient('localhost', 27017)
 db = client.laundry_jungle
-
 app = Flask(__name__)
+app.secret_key = b"kraftonjungle"
 
+@app.route('/')
+def login():
+    if 'user_id' in session:
+        user_id = session['user_id']
+        return redirect('/main')
+    else:
+        return redirect('/signin')
 
-@app.route("/")
+@app.route('/signin', methods=['GET', 'POST'])
 def signin():
-    return render_template("signin.html")
+    if request.method == 'GET':
+        return render_template('signin.html')
+    else:
+        user_id = request.form.get('user_id')
+        password = request.form.get('password')
 
+        user = db.users.find_one({'user_id':user_id})
+        # Nonetype error 방지, 패스워드 일치 여부 확인
+        if user_id and password and user and user['password'] == password:
+            session['user_id'] = user_id
+            return redirect('/main')
+        else:
+            return render_template('signin.html')
 
-@app.route("/signup")
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+    if request.method == 'GET':
+        return render_template('signup.html')
+    else:
+        user_id = request.form.get('user_id')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        sex = request.form.get('sex')
+        team = request.form.get('team')
+        room = request.form.get('room')
+        phone = request.form.get('phone')
+        
+        user = {'user_id':user_id, 'username':username, 'password':password, 'sex':sex, 'team':team, 'room':room, 'phone':phone, 'washer':False, 'dryer':False}
+        if db.users.find_one({'user_id':user_id}) is not NoneType:
+            return render_template('signup.html')
+        # user_id 중복검사
+        # phone 숫자만, 중복 검사
+        else:
+            db.users.insert_one(user)
+            return redirect('/signin')
 
-
-# db.users.insert_one({"user_id": "kristi8041", "username": "염혜지", "password": "1111", "sex": "female", "team": "blue", "room": "220", "phone": "01036758041", "washer": False, "dryer": False})
-# db.users.insert_one({"user_id": "hieronimus92", "username": "손창한", "password": "1111", "sex": "male", "team": "blue", "room": "510", "phone": "01022224444", "washer": False, "dryer": False})
-# db.users.insert_one({"user_id": "hyunhong1012", "username": "이현홍", "password": "1111", "sex": "male", "team": "blue", "room": "513", "phone": "01011113333", "washer": False, "dryer": False})
-# db.users.insert_one({"user_id": "hana1111", "username": "김하나", "password": "1111", "sex": "female", "team": "blue", "room": "217", "phone": "01044445555", "washer": False, "dryer": False})
-
-
-@app.route("/main")
+@app.route('/main', methods=['GET', 'POST'])
 def main():
     machine_list = list(db.machine.find({}, {"_id": 0}).sort("machine_id"))
     user = db.users.find_one({"user_id": "hyunhong1012"})
-    # machine_list[0] == 'a_325', machin_list[1] == 'a_326', machine_list[2] == 'b_325', machine_list[3] == 'b_326'
     return render_template("main.html", machine_list=machine_list, user=user["user_id"])
-
 
 @app.route("/reservation")
 def book():
     request.user
     user_list = db.users.find({"user_id":})
 
-
-if __name__ == "__main__":
-    app.run("0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
